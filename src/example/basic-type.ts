17d7f06e2fef601ea1cb1ba253a6aceed9b4a5d1
zhouhuafei <1123486116@qq.com>
2021-02-06 20:42:43 +0800

    feat: 🎸 basic-type.ts

--- src/example/basic-type.ts
+++ src/example/basic-type.ts
@@ -1,5 +1,5 @@
 {
-  console.log('-----------------------------------------------')
+  console.log('-----------------------')
   let boolean: boolean = false
   console.log('let boolean: boolean\n', boolean)
   let array: number[] = [1, 2, 3]
@@ -8,9 +8,13 @@
   console.log('let array2: Array<string>\n', array2)
   let array3: (number | string)[] = [1, 2, 3, 'a', 'b', 'c']
   console.log('let array3: (number | string)[]\n', array3)
-  let array4: Array<number | string> = [1, 2, 3, 'a', 'b', 'c']
-  console.log('let array4: Array<number | string>\n', array4)
+  let array4: number | string[] = ['a', 'b', 'c']
+  console.log('let array4: number | string[]\n', array4)
+  let number5: number | string[] = 123
+  console.log('let number5: number | string[]\n', number5)
+  let array6: Array<number | string> = [1, 2, 3, 'a', 'b', 'c']
+  console.log('let array6: Array<number | string>\n', array6)
   // 元组
-  let array5: [string, number, boolean] = ['a', 1, true]
-  console.log('let array5: [string, number, boolean]\n', array5)
+  let array7: [string, number, boolean] = ['a', 1, true]
+  console.log('let array7: [string, number, boolean]\n', array7)
 }
