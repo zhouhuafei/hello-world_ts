552388990f7b958404da9b08856c9623ccbd8c58
zhouhuafei <1123486116@qq.com>
2021-02-06 21:06:01 +0800

    feat: 🎸 enum、void、never

--- src/example/basic-type.ts
+++ src/example/basic-type.ts
@@ -17,4 +17,22 @@
   // 元组
   let array7: [string, number, boolean] = ['a', 1, true]
   console.log('let array7: [string, number, boolean]\n', array7)
+
+  // 枚举
+  enum ENUM {
+    START = 1,
+    RUNNING = 5,
+    END
+  }
+
+  console.log('ENUM.RUNNING', ENUM.RUNNING)
+  console.log('ENUM[5]', ENUM[5])
+
+  // void
+  console.log('函数没有返回值则默认返回值是undefined则返回值应为void类型，undefined可以赋值给void类型')
+
+  // never
+  console.log('函数抛错则函数的返回值应为never类型')
+  console.log('函数里是死循环则函数的返回值应为never类型')
+  console.log('never类型的值可以赋值给其他任意类型，但是其他任意类型的值不能赋值给never')
 }
